e843b388c19b8bc5d60b060f655d9e007e249d84
KACH0o0 <91015037+KACH0o0@users.noreply.github.com>
2025-03-01 19:09:17 +0400

    Update server.py

--- server.py
+++ server.py
@@ -61,4 +61,4 @@ def send_message(recipient_id, message):
 
 # 5️⃣ Run the Server
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True))
